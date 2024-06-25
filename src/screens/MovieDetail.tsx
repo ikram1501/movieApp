c760b5f5564b28ffeaaea33eed9fa3ba7b7173d6
Ikram <ikram.ram1501@gmail.com>
2024-06-25 15:57:48 +0700

    4. Akses API

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,12 +1,40 @@
 // src/screens/MovieDetail.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigations/HomeStackNavigation';
+import { API_URL, API_ACCESS_TOKEN } from '@env';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'MovieDetail'>;
 
 const MovieDetail: React.FC<Props> = ({ navigation }) => {
+  const fetchData = (): void => {
+    if (!API_URL || !API_ACCESS_TOKEN) {
+      throw new Error('ENV not found');
+    }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    };
+
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Movie Detail Screen</Text>
